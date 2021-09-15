ba1fcf1d71527ebe6b91365c5f470ea05836c04d
nunnaganesh <90764533+nunnaganesh@users.noreply.github.com>
2021-09-15 23:17:40 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+$('#profile-ripple').ripples({
+resolution:512,
+dropRadius:10
+
+});
+
+  const bars = document.querySelectorAll('.progress-bar');
+bars.forEach(function(bar) {
+  let percentage = bar.dataset.percent;
+  let tooltip = bar.children[0];
+  tooltip.innerText = percentage + '%';
+  bar.style.width = percentage +'%';
+  console.log(percentage);
+
+})
+
+});
